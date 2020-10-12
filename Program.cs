a244588f4c2b04fe8af7e29ab1c39113404564f1
kartiknawal <kartiknawal.7@gmail.com>
2020-10-12 08:48:30 +0530

    [kartik] Add. functionality for creating a board

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace TicTacToe
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tik Tak Toe program!");
+            TicTacToe ticTacToe = new TicTacToe();
+            ticTacToe.CreateBoard();
         }
     }
 }
