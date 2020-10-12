4d74b911934413c6320d05f318ccef6327f490c8
kartiknawal <kartiknawal.7@gmail.com>
2020-10-12 08:59:26 +0530

    [kartik] Add. move to location

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -6,7 +6,11 @@ namespace TicTacToe
 {
     class TicTacToe
     {
-        char[] board = new char[10];
+        char[] board;
+        public TicTacToe()
+        {
+            board = new char[10];
+        }
         public void CreateBoard()
         {
             for (int i = 1; i <= 9; i++)
@@ -27,6 +31,25 @@ namespace TicTacToe
 
             PlayerChoice();
             return ' ';
+        }
+        public void MoveToLocation(char letter)
+        {
+            Console.WriteLine("Enter Location index from 1 to 9");
+            int location = Convert.ToInt32(Console.ReadLine());
+            if (location < 1 || location > 9)
+            {
+                Console.WriteLine("Not valid index");
+                MoveToLocation(letter);
+            }
+            else if (board[location] == ' ')
+            {
+                board[location] = letter;
+            }
+            else
+            {
+                Console.WriteLine("Location already filled");
+                MoveToLocation(letter);
+            }
         }
         public void ShowBoard()
         {
